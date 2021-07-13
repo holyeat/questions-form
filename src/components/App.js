82c50ade276974079c7878ce99007c870e31313f
Yatsenko <webfay1@gmail.com>
2021-07-14 00:02:33 +0300

    Initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,23 @@
 import React from 'react'
-import ProxyBindingsCreate from '../containers/ProxyBindingsCreate';
-import ProxyBindingsList from '../containers/ProxyBindingsList';
+import Questions from '../containers/Questions';
+import reducer from '../reducers/reducer';
+import { createStore } from 'redux';
 
 
 class App extends React.Component {
+    initialState = {};
 
     constructor(props)
     {
         super(props);
-        this.state = {
-            'proxyBindings': []
-        };
-        this.proxyCreateBindingState = {
-            'lastRealAddress': '',
-        };
-        this.proxyBindingCreateComponent = null;
-    }
 
-    rerender(proxyBindings) {
-        const length = Object.keys(proxyBindings.attributes.real_addresses).length;
-        if (length > 1) {
-            const realAddress = proxyBindings.attributes.real_addresses;
-            proxyBindings.attributes.real_addresses[Object.keys(realAddress)[length -1]] = Object.values(realAddress)[length -1];
-        }
-
-
-        if (length > 1) {
-            console.log(length, proxyBindings.attributes.real_addresses);
-            this.state.proxyBindings = this.state.proxyBindings.reduce(function (acc, item) {
-                if (item.id === proxyBindings.id) {
-                    acc.push(proxyBindings);
-                } else {
-                    console.log(item);
-                    acc.push(item);
-                }
-
-                return acc;
-            }, []);
-            console.log(this.state.proxyBindings);
-        } else {
-            console.log('t');
-            this.state.proxyBindings.push(proxyBindings);
-        }
-
-        this.proxyBindingsListComponent.load();
-
-        this.forceUpdate();
+        this.state = createStore(reducer);
     }
 
     render() {
         return (
             <div>
-                <ProxyBindingsList state={this.state} parent={this}/>
-                <ProxyBindingsCreate parent={this} state={this.proxyCreateBindingState}/>
+                <Questions state={this.state} parent={this}/>
           </div>        
         );
     }
