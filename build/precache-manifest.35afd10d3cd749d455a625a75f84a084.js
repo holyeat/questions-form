301cd771056fb3ae6d40a32f594dd19f1f68b4fa
Yatsenko <webfay1@gmail.com>
2021-07-28 14:54:17 +0300

    Deploy implement

---
+++ build/precache-manifest.35afd10d3cd749d455a625a75f84a084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cbef3cd6589fd19b5893b2b729a267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee6d28af42da16ffe871",
+    "url": "/static/js/2.f5ec5d91.chunk.js"
+  },
+  {
+    "revision": "a3ef6a11e11ca6a16b435c3d2be41ee9",
+    "url": "/static/js/2.f5ec5d91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47ea224c7b97f5bb2923",
+    "url": "/static/js/main.9998d77b.chunk.js"
+  },
+  {
+    "revision": "b37bfed06aa9fc01a2d0",
+    "url": "/static/js/runtime-main.1fddad14.js"
+  }
+]);
