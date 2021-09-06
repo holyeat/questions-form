868fec98fb43158a2c983933664273d67c857183
Yatsenko <webfay1@gmail.com>
2021-09-07 00:25:15 +0300

    Fix redirect for form filler

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29586abd6616cfdd513189e8ffe57383.js"
+  "/precache-manifest.d9ee554caf110c60af9c8f70dddf5396.js"
 );
 
 self.addEventListener('message', (event) => {
