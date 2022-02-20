9e702cbb44490b18e11bf6b237ca372659cc7455
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2022-02-21 00:40:38 +0200

    Fix constants

---
+++ build/precache-manifest.0f5a61c5bcc67e4dd72ec477a97eb734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f971d4032142a4cd368bde0bf241198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "507ac8f08227110643e6",
+    "url": "/static/js/2.22ef6777.chunk.js"
+  },
+  {
+    "revision": "a3ef6a11e11ca6a16b435c3d2be41ee9",
+    "url": "/static/js/2.22ef6777.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15639be51af267a3ba06",
+    "url": "/static/js/main.54a7128e.chunk.js"
+  },
+  {
+    "revision": "b37bfed06aa9fc01a2d0",
+    "url": "/static/js/runtime-main.1fddad14.js"
+  }
+]);
